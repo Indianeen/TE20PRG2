0d84eb931e782eb000199dfeef81235904ca3fd5
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-08-26 13:14:36 +0200

    Övning 1 Jag har skrivit grundkoden och inledande samt avslutande meddelande. Det svåra är att komma fram till hur man gör random uttryck

--- src/ovning1/övning1.java
+++ src/ovning1/övning1.java
@@ -1,14 +1,21 @@
 package ovning1;
 
 import javax.swing.*;
+import java.util.Random;
 
 public class övning1 {
     public static void övning1 (){
+        int i = 1;
         String start = JOptionPane.showInputDialog("Hej. Vad vill du prata om");
         if (start == ""){
-            JOptionPane.showMessageDialog(null, "Så du har ingenting att prata om? Ha en bra dag!");
+            JOptionPane.showMessageDialog(null, "Så du har ingenting att prata om?");
         }else {
-
+            while (i == 1){
+                String respons = Random ;
+                String convo = JOptionPane.showInputDialog(respons);
+                if (convo == ""){i = 0;};
+            };
+            JOptionPane.showMessageDialog(null, "Ha en bra dag");
         }
     }
 }
