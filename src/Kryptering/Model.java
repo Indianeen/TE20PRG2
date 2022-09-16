aeeee23dc7eeba76a5a4fb0cb87ba4606f908de2
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-09-16 13:21:12 +0200

    Huh?

--- src/Kryptering/Model.java
+++ src/Kryptering/Model.java
@@ -2,6 +2,17 @@ package Kryptering;
 
 public class Model {
     public static void main(String[]args){
+        String Encrypted;
+        String Decrypted;
+        String Key;
+
+
+
+    }
+    private class encrypt{
+
+    }
+    private class decrypt{
 
     }
 }
---
+++ src/Kryptering/Model.md
@@ -0,0 +1,15 @@
+Delar: [Input][Key][Encrypted][Decrypted]
+
+Läs [Input]
+
+XOR [Input][Key] -> [Encrypted]
+
+Spara [Key][Encrypted]
+
+Skicka [Encrypted]
+
+Läs [Encrypted]
+
+XOR [Encrypted][Key] -> [Decrypted]
+
+Skicka [Decrypted]
